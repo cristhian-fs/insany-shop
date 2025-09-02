2b9769dfe64179ab00561fea5141c8fff19c4c87
Cristhian <cristhianfernandolp@gmail.com>
2025-09-01 22:24:48 -0300

    style: update page default styles, add normalize css

--- app/styles/theme.ts
+++ app/styles/theme.ts
@@ -5,6 +5,13 @@ export const theme = {
     background: "#f3f7ff",
     foreground: "#000000",
     "muted-foreground": "#737380",
+    "header-background": "#ffffff",
+    "header-logo-foreground": "#5D5D6D",
+    // It's the same as background-color and muted-foreground, but I choose to keep it for future reference
+    "header-search-background": "#F3F7FF",
+    "header-search-foreground": "#737380",
+    "bag-button-span-background": "#DE3838",
+    "bag-button-span-foreground": "#ffffff",
   },
   breakpoints,
 } as const;
